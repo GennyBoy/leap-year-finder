e39c25e67fa21946a10f83f477555e1edc9270ed
GennyBoy <tassie2856@gmail.com>
2023-06-15 20:13:37 +0900

    isLeapYearメソッドを追加

---
+++ main.js
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+
+const isLeapYear = (year) => {
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+}
+
+export {isLeapYear};
---
+++ test/main_test.js
@@ -0,0 +1,11 @@
+import assert from 'node:assert';
+import test from 'node:test';
+import {isLeapYear} from './../main.js';
+
+test('return true if it is a leap year', t => {
+  assert.strictEqual(isLeapYear(2000), true);
+});
+
+test('return false if it is not a leap year', t => {
+  assert.strictEqual(isLeapYear(2001), false);
+});
