55bbe26cf63cf44783d195fcf567cc9bcc4d0529
GennyBoy <tassie2856@gmail.com>
2023-10-19 20:10:14 +0900

    Delete code for calling each methods from CLI
    The main use case is calling these methods from another JavaScript code.
    So there is no need to be able to call it from CLI.
    Also, I didn't want to have unnecesarry dependencies in the code.

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 #! /usr/bin/env node
 
-import { program } from "commander";
-
 const isLeapYear = (year) => {
   return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
@@ -77,81 +75,3 @@ export {
   getDateForNextLeapYearLastDayOfFebruary,
   getDateForPreviousLeapYearLastDayOfFebruary,
 };
-
-program
-  .command("isLeapYear [year]")
-  .description("Check if a given year is a leap year")
-  .action((year) => {
-    console.log(`getNextLeapYear(${year})`);
-    console.log(isLeapYear(year));
-  });
-
-program
-  .command("isCurrentYearLeap")
-  .description("Check if the current year is a leap year")
-  .action(() => {
-    console.log(isCurrentYearLeap());
-  });
-
-program
-  .command("getNextLeapYear [year]")
-  .description(
-    "Get the next leap year after the given year (or the current year if not specified)"
-  )
-  .action((year) => {
-    console.log(getNextLeapYear(year));
-  });
-
-program
-  .command("getPreviousLeapYear [year]")
-  .description(
-    "Get the previous leap year before the given year (or the current year if not specified)"
-  )
-  .action((year) => {
-    console.log(getPreviousLeapYear(year));
-  });
-
-program
-  .command("getNextLeapYears <number> [year]")
-  .description(
-    "Get the next N leap years after the given year (or the current year if not specified)"
-  )
-  .action((number, year) => {
-    console.log(getNextLeapYears(number, year));
-  });
-
-program
-  .command("getPreviousLeapYears <number> [year]")
-  .description(
-    "Get the previous N leap years before the given year (or the current year if not specified)"
-  )
-  .action((number, year) => {
-    console.log(getPreviousLeapYears(number, year));
-  });
-
-program
-  .command("getDateForLastDayOfFebruary <year>")
-  .description("Get the date for the last day of February for a given year")
-  .action((year) => {
-    console.log(getDateForLastDayOfFebruary(year));
-  });
-
-program
-  .command("getDateForNextLeapYearLastDayOfFebruary")
-  .description(
-    "Get the date for the last day of February for the next leap year after the current year"
-  )
-  .action(() => {
-    console.log(getDateForNextLeapYearLastDayOfFebruary());
-  });
-
-program
-  .command("getDateForPreviousLeapYearLastDayOfFebruary")
-  .description(
-    "Get the date for the last day of February for the previous leap year before the current year"
-  )
-  .action(() => {
-    console.log(getDateForPreviousLeapYearLastDayOfFebruary());
-  });
-
-program.parse(process.argv);
